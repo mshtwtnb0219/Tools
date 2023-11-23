65e0aaee2d88a111ddbad9163c77fd50fc8658af
mshtwtnb0219 <b77yellow@gmail.com>
2023-11-23 23:05:01 +0900

    ｺﾐｯﾄ

---
+++ SQL抜粋マクロ/サンプル/f.java
@@ -0,0 +1,21 @@
+
+pakage a.a.a........
+import java.util.lang...
+
+
+public class a {
+
+	private static void main(Stinrg args[]){
+
+	StirngBuilder sql = new StirngBuilder();
+	sql.apend("SELECT * ");
+	sql.apend("FROM table as a ");
+	sql.apend("WHERE a.id = ?");
+	sql.apend("ORDER BY 1,2,3,4,5,6,7 ");
+	
+
+	}
+
+
+
+}
